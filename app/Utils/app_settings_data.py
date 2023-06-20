2bfd6c61595d21e6f4d4b3ffff15a0cc38dc4907
Lakusan <Andreas.Lakus@googlemail.com>
2023-06-20 21:49:55 +0200

    typo fixed

--- app/Utils/app_settings_data.py
+++ app/Utils/app_settings_data.py
@@ -10,5 +10,26 @@ class AppSettings:
     language: str
     current_cam_index: int = None
     available_cams_index: List[int] = None
+    
+def get_app_name(self) -> str:
+    return self.app_name
+
+def get_version(self) -> str:
+    return self.version()
+
+def get_debug_mode(self) -> bool:
+    return self.debug_mode
+
+def get_theme(self) -> str : 
+    return self.theme   
+
+def get_language(self) -> str:
+    return self.language
+
+def get_current_cam_index(self) -> int:
+    return self.current_cam_index
+
+def get_available_cams_index(self) -> List[int]:
+    return self.available_cams_index
 
     
