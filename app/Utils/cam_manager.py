fd1ffa0fe34e70c24547b4f9aa4ed93688ac934d
Lakusan <Andreas.Lakus@googlemail.com>
2023-06-29 17:22:03 +0200

    minor changes on camera manager

--- app/Utils/cam_manager.py
+++ app/Utils/cam_manager.py
@@ -4,15 +4,24 @@ from typing import List
 class CameraManager:
     _instance = None
     
-    def __new__(cls):
+    @classmethod
+    def __new__(cls, *args, **kwargs):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
         return cls._instance
     
+    @classmethod
     def __init__(self):
         self.cameras = []
         self.cameraIndex = None
+        
+    @classmethod
+    def get_instance(cls):
+        if not cls._instance:
+            cls._instance = ()
+        return cls._instance
 
+    @classmethod
     def find_available_cameras(self) -> bool:
         self.cameras = []
         index = 0
@@ -24,12 +33,14 @@ class CameraManager:
             cap.release()
             index += 1
         if len(self.cameras) != 0:
-            return True
+                return True
         else:
             raise Exception("No cameras found")
 
+    @classmethod
     def get_available_cameras(self) -> List[int]:
         return self.cameras
     
+    @classmethod
     def get_camera_index(self) -> int:
         return self.cameraIndex
