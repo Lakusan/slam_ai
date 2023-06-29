f33f08abb04cc652105e2dc9ff2ff8696acf1a0f
Lakusan <Andreas.Lakus@googlemail.com>
2023-06-29 17:18:56 +0200

    GUI loading.py added loading screen, added label to show found cameras, find attached cameras

--- app/GUI/loading.py
+++ app/GUI/loading.py
@@ -4,6 +4,8 @@ import threading
 import time
 from ..Utils.cam_manager import CameraManager
 
+# TODO change camera_manager to Singleton -> then add to service_loader
+
 class LoadingScreen(tk.Frame):
     def __init__(self, master): 
         super().__init__(master)
@@ -21,18 +23,29 @@ class LoadingScreen(tk.Frame):
         self.progress_bar.pack(pady=10)
     
     def start_loading(self):
-        self.thread = threading.Thread(target=self.run_tasks)
-        self.thread.start()
+        self.main_thread = threading.Thread(target=self.run_tasks)
+        self.main_thread.start()
+        self.loading_thread = threading.Thread(target=self.loading_bar)
+        self.loading_thread.start()
         
+    
+    def loading_bar(self):
+        val = 0
+        for i in range(6):
+            time.sleep(1)
+            val = val + 10  + i
+            self.progress_var.set(val)
+    
     def run_tasks(self):
         # find all active usb cameras
         self.loading_label.config(text="Looking for USB Web Cams")
         try:
             self.camera_manager.find_available_cameras()
+            self.progress_var.set(100)
         except Exception as e:
             self.loading_label.config(text="No USB-Camera found. Please connect one and restart the app.")
 
-        cameras_found = "Found " + str(len(self.camera_manager.get_availabe_cameras())) + " Camera(s)"
+        cameras_found = "Found " + str(len(self.camera_manager.get_available_cameras())) + " Camera(s)"
         self.loading_label.config(text=cameras_found)
         time.sleep(2)
         self.loading_label.config(text="Stuff Loaded! Redirecting to Main Menu...")
